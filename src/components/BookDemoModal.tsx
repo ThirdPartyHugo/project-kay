58bbb167be9cc0fd9d9e97c2d902729e7b86552d
ThirdPartyHugo <hpskate26@gmail.com>
2024-12-01 16:40:03 -0500

    dd

--- src/components/BookDemoModal.tsx
+++ src/components/BookDemoModal.tsx
@@ -128,6 +128,6 @@ export const BookDemoModal = ({ onClose, selectedPackage }: BookDemoModalProps) 
     
 <div class="calendly-inline-widget" data-url="https://calendly.com/theaffiliateshq/30min" style="min-width:320px;height:700px;"></div>
 <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-<!-- Calendly inline widget end -->
+
   );
 }
