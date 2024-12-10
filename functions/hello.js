935d26e85657bf373be31c49456f87697095076b
ThirdPartyHugo <201945297@cegepsherbrooke.qc.ca>
2024-12-10 13:42:03 -0500

    d

---
+++ functions/hello.js
@@ -0,0 +1,8 @@
+// path/to/your/functions/hello.js
+exports.handler = async function(event, context) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello, World!" }),
+    };
+  };
+  
